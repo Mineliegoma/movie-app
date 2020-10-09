9f54edb36121d0f02fa71ab3a7b2f68ff7271fdd
“Mineliegoma” <garlandegoma@gmail.com>
2020-10-09 05:55:17 -0400

    addded event on like button

--- react_practice/src/MovieCard.js
+++ react_practice/src/MovieCard.js
@@ -1,21 +1,49 @@
 import React from 'react';
-function MovieCard(props) {
- return (
-  <div class="container">
-   <div class="card-front">
-    <h2 class="title-card">{props.data.title}</h2>
-    <img alt="" src={props.data.imageUrl} class="image" />
-    <div class="likes-section">
-     <span class="number-likes">0 likes</span>
-     <button class="button">👍</button>
-    </div>
-    <div class="more-infos">
-     <p>{props.data.genre}</p>
-     <p>{props.data.year}</p>
-    </div>
-   </div>
-  </div>
+class MovieCard extends React.Component {
+ state = {
+  likes: this.props.data.likes
+ }
+ incrementLikes = () => {
+  //console.log(this.props.data.likes)
+  this.setState(previousState => {
+   return {
+    likes: parseInt(previousState.likes) + 1
 
- )
+   }
+  })
+
+ }
+
+
+ clickHandler = () => {
+  console.log('im clicking')
+  this.incrementLikes(this.state)
+ }
+ movieLikes = () => {
+  console.log(this.state.likes)
+  return this.state.likes
+ }
+
+ render() {
+  return (
+   <>
+    <div class="container">
+     <div class="card-front">
+      <h2 class="title-card">{this.props.data.title}</h2>
+      <img alt="" src={this.props.data.imageUrl} class="image" />
+      <div class="likes-section">
+       <span class="number-likes">{this.movieLikes()} likes</span>
+       <button onClick={this.clickHandler} class="button">👍</button>
+      </div>
+      <div class="more-infos">
+       <p>{this.props.data.genre}</p>
+       <p>{this.props.data.year}</p>
+      </div>
+     </div>
+    </div>
+   </>
+
+  )
+ }
 }
 export default MovieCard
--- react_practice/src/api.js
+++ react_practice/src/api.js
@@ -5,6 +5,7 @@ const movies = [
   imageUrl: "https://resizing.flixster.com/h8e7W7cVaQhuLdSvABDkJk6r5sc=/206x305/v1.bTsxMTE2NjE5OTtqOzE4MzU0OzEyMDA7ODAwOzEyMDA",
   year: 1975,
   genre: "Horror",
+  likes: "0"
 
  },
  {
@@ -13,6 +14,7 @@ const movies = [
   imageUrl: "https://imgc.allpostersimages.com/img/print/u-g-F4S5W20.jpg?w=550&h=550&p=0",
   year: 1999,
   genre: "Science Fiction",
+  likes: "0"
 
  },
  {
@@ -21,7 +23,7 @@ const movies = [
   imageUrl: "https://images-na.ssl-images-amazon.com/images/I/515DYf99zfL.jpg",
   year: 1985,
   genre: "Adventure",
-
+  likes: "0"
  },
  {
   "id": 4,
@@ -29,6 +31,7 @@ const movies = [
   imageUrl: "https://upload.wikimedia.org/wikipedia/en/thumb/b/b5/Free_willy.jpg/220px-Free_willy.jpg",
   year: 1993,
   genre: "Family",
+  likes: "0"
 
  },
  {
@@ -37,7 +40,7 @@ const movies = [
   imageUrl: "https://m.media-amazon.com/images/M/MV5BZjQxYTA3ODItNzgxMy00N2Y2LWJlZGMtMTRlM2JkZjI1ZDhhXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_.jpg",
   year: 1986,
   genre: "Action",
-
+  likes: "0"
  },
  {
   "id": 6,
@@ -45,6 +48,7 @@ const movies = [
   imageUrl: "https://img01.mgo-images.com/image/thumbnail?id=1MV270609a1c6c89af5538a6d63cea71ed4&ql=70&sizes=310x465",
   year: 2004,
   genre: "Comedy",
+  likes: "0"
 
  },
  {
@@ -53,6 +57,7 @@ const movies = [
   year: "1980",
   imageUrl: "https://images-na.ssl-images-amazon.com/images/I/41npElxA48L._AC_.jpg",
   genre: "Science Fiction",
+  likes: "0"
  },
  {
   "id": 8,
@@ -60,6 +65,7 @@ const movies = [
   imageUrl: "https://www.movieartarena.com/imgs/wab.jpg",
   year: 1991,
   genre: "Comedy",
+  likes: "0"
  },
  {
   "id": 9,
@@ -67,7 +73,7 @@ const movies = [
   year: "1987",
   genre: "Drama",
   imageUrl: "https://images-na.ssl-images-amazon.com/images/I/51X9TJrRBCL._AC_.jpg",
-
+  likes: "0"
  },
  {
   "id": 10,
@@ -75,7 +81,7 @@ const movies = [
   title: "Alien",
   genre: "Horror",
   imageUrl: "https://i.etsystatic.com/14461980/r/il/28c6d5/1417485862/il_570xN.1417485862_le2x.jpg",
-
+  likes: "0"
  },
  {
   "id": 11,
@@ -83,7 +89,7 @@ const movies = [
   title: "Predator",
   genre: "Horror",
   imageUrl: "https://movieposters2.com/images/1566923-b.jpg",
-
+  likes: "0"
  }
 ]
 export default movies 
